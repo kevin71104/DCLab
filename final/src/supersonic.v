854e62f162ba14bbfd23eb5032967535977a42e2
kevin71104 <b03901026@ntu.edu.tw>
2018-01-16 22:37:16 +0800

    detect posedge of echo

--- final/src/supersonic.v
+++ final/src/supersonic.v
@@ -22,28 +22,29 @@ module supersonic#(
     reg             valid_cur;
     reg             valid_nxt;
 
-    reg             state_cur;  // after trigger high for 500 cycle
-    reg             state_nxt;
-    reg  [     8:0] counter;    // needs at least 500 cycles to trigger
-    reg  [     8:0] counter_nxt;
-    reg             triggerSuc_cur;
-    reg             triggerSuc_nxt;
+    reg  state_cur;  // after trigger high for 500 cycle
+    reg  state_nxt;
+    reg  prev_echo_cur;
+    wire prev_echo_nxt;
+    reg  triggerSuc_cur;
+    reg  triggerSuc_nxt;
     
 //==== combinational circuit
     // output
     assign valid        = valid_cur;
     assign distance     = distance_cur;
     assign triggerSuc   = triggerSuc_cur;
+	
+	assign prev_echo_nxt = echo;
 
     always @ ( * ) begin
         distance_nxt    = distance_cur;
         state_nxt       = state_cur;
         valid_nxt       = 1'b0;
-        counter_nxt     = 9'd0;
         triggerSuc_nxt  = 1'b0;
         case (state_cur)
             1'b0: begin
-                if (counter == 9'd500)begin
+                if (prev_echo_cur ^ echo && echo)begin
                     state_nxt       = 1'b1;
                     distance_nxt    = {TotLen{1'b0}};
                     triggerSuc_nxt  = 1'b1;
@@ -53,26 +54,18 @@ module supersonic#(
                     distance_nxt    = distance_cur;
                     triggerSuc_nxt  = 1'b0;
                 end
-
-                if (counter == 9'd500 | ~trigger)begin
-                    counter_nxt = 9'd0;
-                end
-                else begin
-                    counter_nxt = counter + 1;
-                end
             end
             1'b1: begin
                 triggerSuc_nxt = 1'b0;
                 if (distance_cur != {TotLen{1'b1}})begin
-                    if (echo)begin
-                        distance_nxt= distance_cur + 1;
-                        state_nxt   = state_cur;
-                        valid_nxt   = 1'b0;
-                    end
-                    else begin
-                        distance_nxt= distance_cur + 1;
+					distance_nxt= distance_cur + 1;
+                    if (prev_echo_cur ^ echo && ~echo)begin
                         state_nxt   = 1'b0;
                         valid_nxt   = 1'b1;
+                    end
+                    else begin
+                        state_nxt   = 1'b1;
+                        valid_nxt   = 1'b0;
                     end
                 end
                 else begin
@@ -88,14 +81,14 @@ module supersonic#(
     always @(posedge clk or negedge rst_n) begin
         // asynchronous reset
         if (~rst_n) begin
-            counter        <= 9'b0;
+			prev_echo_cur  <= 1'b0;
             state_cur      <= 1'b0;
             valid_cur      <= 1'b0;
             distance_cur   <= {TotLen{1'b0}};
             triggerSuc_cur <= 1'b0;
         end
         else begin
-            counter        <= counter_nxt;
+			prev_echo_cur  <= prev_echo_nxt;
             state_cur      <= state_nxt;
             valid_cur      <= valid_nxt;
             distance_cur   <= distance_nxt;
